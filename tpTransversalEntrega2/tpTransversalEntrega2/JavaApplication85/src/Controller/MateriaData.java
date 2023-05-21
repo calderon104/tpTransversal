a370914902b109f51af0e826560bf52ef9876a5a
jb-2015 <juan_matias_becerra@hotmail.com>
2023-05-21 00:40:56 -0300

    Avance en los metodos de MateriasData

--- tpTransversalEntrega2/tpTransversalEntrega2/JavaApplication85/src/Controller/MateriaData.java
+++ tpTransversalEntrega2/tpTransversalEntrega2/JavaApplication85/src/Controller/MateriaData.java
@@ -5,10 +5,79 @@
  */
 package Controller;
 
+import Model.Materia;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Gonza
  */
 public class MateriaData {
+    private Connection conn;
+    private Conexion conexion;
+
+    public MateriaData(Conexion conexion) {
+        this.conexion = conexion;
+    }
+    
+    public void guardarMateria(Materia m){
+        String sql= "INSERT INTO materias VALUES (null,?,?,?)";
+        
+        try{
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, m.getNombre());
+            ps.setInt(2, m.getAnio());
+            ps.setBoolean(3, m.isEstado());
+            
+            if(ps.execute()){
+                System.out.println("Materia cargada con exito");
+            }
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error al cargar materia: "+e.toString());
+        }
+    }
+    
+    public Materia buscarMateria(int id){
+        String sql = "SELECT * FROM materias WHERE id=?";
+        try{
+            PreparedStatement ps = conn.prepareCall(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+                Materia m = new Materia(rs.getInt("id_materia"), rs.getString("nombre"), rs.getInt("anio"), rs.getBoolean("estado"));
+                return m;
+            }
+            
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error al intentar buscar materia: "+e.toString());
+        }
+        return null;
+    }
+    public Materia modificarMateria(int id , Materia m){
+        
+        String sql = "UPDATE materias SET id_materia= ?, nombre=?,anio=?,estado=? WHERE id_materia=?";
+        
+        try{
+            PreparedStatement ps = conn.prepareCall(sql);
+            ps.setInt(1,m.getId_materia());
+            ps.setString(2,m.getNombre());
+            ps.setInt(3,m.getAnio());
+            ps.setBoolean(4, m.isEstado());
+            ps.setInt(5, id);
+            
+            if(ps.execute()){
+                System.out.println("Modificacion exitosa");
+            }
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null,"Error al actualizar materia: "+e.toString());
+        }
+        
+        return m;
+    }
     
 }
